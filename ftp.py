57f21207b711cc0cbe8696881d34741705892440
krawat428 <krawat428@gmail.com>
2018-01-31 01:13:24 +0530

    Create ftp.py

---
+++ ftp.py
@@ -0,0 +1,21 @@
+import ftplib
+import os
+
+
+directory=''
+host=''
+username=''
+password=''
+
+
+command1='ls {x1}'.format(x1=directory)
+os.chdir(dire)
+
+
+a=os.popen(command1).read()
+b=a.split()
+ftp = ftplib.FTP(host,username,password)
+for i in b:
+	ftp.storbinary('STOR {x}'.format(x=i), open(i, 'rb'))
+ftp.quit()
+
